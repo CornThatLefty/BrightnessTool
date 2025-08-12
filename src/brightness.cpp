7b60f73d20eaa0635f2b1e7221e90e1af0d4b500
Chuck <lefthandedcorn@gmail.com>
2025-08-11 23:33:43 -0700

    Function cleanup

--- src/brightness.cpp
+++ src/brightness.cpp
@@ -1,10 +1,25 @@
-#include <cstdlib>
 #include <fstream>
 #include <iostream>
 #include <string>
 
-std::string const currentBrightnessPath = "/sys/class/backlight/intel_backlight/brightness";
-std::string const maxBrightnessPath = "/sys/class/backlight/intel_backlight/max_brightness";
+std::string const currentBrightnessPath =
+    "/sys/class/backlight/intel_backlight/brightness";
+std::string const maxBrightnessPath =
+    "/sys/class/backlight/intel_backlight/max_brightness";
+
+void set_int_from_file(std::string path, int content) {
+  std::ofstream file(path);
+  file << content;
+}
+
+int get_int_from_file(std::string path) {
+  std::ifstream file(path);
+
+  std::string fileString;
+  std::getline(file, fileString);
+  int fileInt = std::stoi(fileString);
+  return fileInt;
+}
 
 int main(int argumentCount, char *argValues[]) {
   if (argumentCount <= 1) {
@@ -14,22 +29,16 @@ int main(int argumentCount, char *argValues[]) {
 
   if (argumentCount == 2) {
     int newBrightnessInt = std::stoi(argValues[1]);
-    std::ofstream currentBrightnessFile(currentBrightnessPath);
-    std::ifstream maxBrightnessFile(maxBrightnessPath);
-
-    std::string maxBrightnessString;
-    std::getline(maxBrightnessFile, maxBrightnessString);
-    int maxBrightnessInt = std::stoi(maxBrightnessString);
+    int maxBrightnessInt = get_int_from_file(maxBrightnessPath);
 
     if (newBrightnessInt <= maxBrightnessInt) {
-      currentBrightnessFile << newBrightnessInt;
-      std::cout << "Current Brightness: " << newBrightnessInt << "/" << maxBrightnessInt << std::endl;
+      set_int_from_file(currentBrightnessPath, newBrightnessInt);
+      std::cout << "Current Brightness: " << newBrightnessInt << "/"
+                << maxBrightnessInt << std::endl;
     } else {
       std::cerr << "File open error" << std::endl;
     }
 
-    currentBrightnessFile.close();
-    maxBrightnessFile.close();
     return 0;
   }
 }
